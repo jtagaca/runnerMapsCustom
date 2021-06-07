86f2dccf91f470fb2d5bf261b184aca447dfa87b
jtagaca <jtagaca@csub.edu>
2021-06-06 17:31:59 -0700

    new

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React, {useState, useEffect} from 'react';
+import './App.css';
+import AlgoVisualizer from './components/AlgoVisualizerPage/AlgoVisualizer';
+
+
+
+function App() {
+
+  
+  return (
+    <div className="App">
+      <AlgoVisualizer />
+    </div>
+  );
+}
+
+export default App;
